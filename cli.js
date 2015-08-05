27f2cb660e8c3b6cc28951e246ecb36072b90d2a
nareix <hi.nareix@gmail.com>
2015-08-05 14:34:41 +0800

    output JSON

--- cli.js
+++ cli.js
@@ -3,7 +3,7 @@ require('./index.js')({
 	weixinChanId: process.argv[2],
 	page: parseInt(process.argv[3]) || 1,
 }).then(function (r) {
-	console.log(r);
+	process.stdout.write(JSON.stringify(r));
 }, function (e) {
 	console.log('usage: node cli.js <weixinChanId> <page>');
 	console.log(e.stack);
