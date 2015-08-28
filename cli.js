2e06cd212bca4b7fbca013e17068ac114d869613
nareix <hi.nareix@gmail.com>
2015-08-28 13:57:24 +0800

    change timeout method to process.exit

--- cli.js
+++ cli.js
@@ -1,22 +1,18 @@
 
 var argv = require('minimist')(process.argv.slice(2));
 
-var tasks = [require('./index.js')({
+if (argv.t)
+	setTimeout(function () {
+		process.stderr.write('timeout\n');
+		process.exit(1);
+	}, argv.t*1000);
+
+require('./index.js')({
 	weixinChanId: argv._[0],
 	page: parseInt(argv._[1]) || 1,
-})];
-
-var timer;
-if (argv.t)
-	tasks.push(new Promise(function (_, reject) {
-		timer = setTimeout(reject, argv.t*1000, new Error('timeout'));
-	}));
-
-Promise.race(tasks).then(function (r) {
+}).then(function (r) {
 	process.stdout.write(JSON.stringify(r));
-	clearTimeout(timer);
 }, function (e) {
-	clearTimeout(timer);
 	process.stderr.write('usage: node cli.js <weixinChanId> <page> -t <timeoutSeconds>\n');
 	process.stderr.write(e.stack);
 });
